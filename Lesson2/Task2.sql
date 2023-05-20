1442bf7eeeb23268f4291898ff7c3a586a20a57b
Taras <thezulkin@gmail.com>
2023-05-20 18:04:52 +0200

    work
    home

---
+++ Lesson2/Task2.sql
@@ -0,0 +1,89 @@
+drop database storage;
+create database Storage;
+
+use storage;
+
+-- Створюємо таблиці ---------------------------------------
+create table Personnel(
+id_personal int auto_increment,
+name varchar(20),
+position varchar(20),
+primary key(id_personal));
+
+create table Distributor(
+id_distributor int auto_increment,
+Name_Distributor varchar(30) unique,
+Product varchar(20) unique,
+number int,
+primary key(id_distributor));
+
+create table RegularCostumer(
+id_costumer int auto_increment,
+name_costumer varchar(30) unique,
+Product varchar(20),
+Number int,
+primary key (id_costumer));
+
+create table InStorage(
+id int auto_increment,
+date datetime default current_timestamp,
+worker int,
+name_company varchar(30),
+name_costumer varchar(30),
+product varchar(20),
+operation varchar(20),
+primary key(id));
+
+-- Дивимось створені таблиці	-------------------------------------
+select * from distributor;
+select * from regularcostumer;
+select * from personnel;
+select * from instorage;
+-- -------------------------------------------------------
+
+-- Створюємо заязки --------------------------------------
+alter table instorage
+add constraint worker
+foreign key (worker) references personnel(id_personal);
+
+alter table instorage
+add constraint name_company
+foreign key(name_company) references distributor(name_distributor);
+
+alter table instorage
+add constraint name_costumer
+foreign key(name_costumer) references RegularCostumer(name_costumer);
+
+alter table instorage
+add constraint product
+foreign key(product) references distributor(product);
+
+-- Заповнення таблиць -------------------------------------------
+insert into personnel(name, position)
+values('Ivan', 'Manager'),
+('Petro', 'Worker'),
+('Oleg', 'Worker');
+
+insert into Distributor(Name_Distributor, Product, number)
+values ('Panasonic', 'TV', '20'),
+('Samsung', 'Smartphone','30'),
+('LG', 'Vacuumclener','25'),
+('Apple', 'Iphone','10');
+
+insert into RegularCostumer(Name_costumer, product, number)
+values ('Bilka', 'TV', '10'),
+('Foxtrot', 'Iphone', '5'),
+('Eldorado', 'Vacuumclener', '20');
+
+insert into instorage(worker, name_company, name_costumer, product, operation)
+values ('1', 'LG', 'Bilka', 'Vacuumclener', 'unloading/receiving'),
+('2', 'Apple', 'Foxtrot', 'Iphone', 'loading'),
+('3', 'Samsung', 'Eldorado', 'Smartphone', 'loading'),
+('2', 'Panasonic', 'Bilka', 'TV', 'unloading'),
+('1', 'Panasonic', 'Foxtrot', 'TV', 'receiving');
+
+-- Дивимось створені таблиці	-------------------------------------
+select * from distributor;
+select * from regularcostumer;
+select * from personnel;
+select * from instorage;
---
+++ Lesson2/task4.sql
@@ -0,0 +1,80 @@
+drop database Department_office;
+create database Department_office;
+use Department_office;
+
+create table departments(
+id int auto_increment,
+name varchar(30),
+department_phone varchar(20),
+department_code varchar(4),
+CONSTRAINT DP_unique UNIQUE (department_code, name),
+primary key(id));
+
+create table staff_schedule(
+id int auto_increment,
+name_position varchar(25) unique,
+code_position smallint unique,
+education varchar(30),
+primary key(id));
+
+create table workers(
+id int auto_increment,
+department_code varchar(4),
+code_position smallint,
+name varchar(30),
+phone int,
+salary int,
+foreign key(department_code) references departments(department_code),
+foreign key(code_position) references staff_schedule(code_position),
+primary key(id));
+
+create table workers_data(
+worker_id int auto_increment,
+foreign key(worker_id) references workers(id),
+family_status varchar(10),
+childrens tinyint,
+vacation_start date default "0000/00/00",
+vacation_finish date default "0000/00/00",
+primary key (worker_id)
+);
+
+-- Створені таблиці --------------------------------------------
+select * from departments;
+select * from staff_schedule;
+select * from workers;
+select * from workers_data;
+
+-- Зповнення таблиць-------------------------------------------
+insert into departments(name, department_phone, department_code)
+values ('Bugaltery', '5248931', '1546'),
+('Industrial', '5688491', '1856');
+
+insert into staff_schedule(name_position, code_position, education)
+values('Maneger', 23, 'Profile'),
+('Practicant', 12, '-'),
+('Higth worker', 3, 'Profile'),
+('worker', 2, 'Semi-profile');
+
+insert into workers(department_code, code_position, name, phone, salary)
+values ('1546','23', 'Ivan', 974525869, 20000),
+('1856','12', 'Vova', 984585569, 2000),
+('1856','2', 'Nils', 951564896, 12000),
+('1546','2', 'Ivan', 685214986, 13000),
+('1856','3', 'Sara', 975493132, 15000),
+('1546','3', 'Masha', 974525869, 15000);
+
+insert into workers_data(family_status, childrens, vacation_start, vacation_finish)
+values ('Single', '0', "2023/09/01", "2023/09/20"),
+('Single', '2', "0000/00/00", "0000/00/00"),
+('Marige', '1', "2022/12/25", "2023/01/10"),
+('Marige', '2', "0000/00/00", "0000/00/00"),
+('Marige', '0', "0000/00/00", "0000/00/00"),
+('Marige', '1', "2023/03/01", "2023/04/01");
+
+
+
+-- Створені таблиці --------------------------------------------
+select * from departments;
+select * from staff_schedule;
+select * from workers;
+select * from workers_data;
